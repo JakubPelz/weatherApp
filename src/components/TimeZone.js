8899acf29a240372329bae80b87c9753fa4713eb
Jakub Pelz <JakubMac@Jakub-MacBook-Air.local>
2021-10-29 15:45:04 +0200

    Value problem solved

--- src/components/TimeZone.js
+++ src/components/TimeZone.js
@@ -7,13 +7,13 @@ const TimeZone = () => {
    
     return (
                 <div className="place-container">
-                    <div className="time-zone">{/* {weather.data.name}{console.log(weather.data.name.length)} */}<span style={{ fontSize: "bolt" }}> {/* {weather.data.sys.country} */}</span></div>
-                    <div className="country">Longitude is: {/* {weather.data.coord.lon} */}</div>
-                    <div className="country">Latitude is:{/*  {weather.data.coord.lat} */}</div>
+                    <div className="time-zone">{weather.data === null ? 'Pleas select a City ' : weather.data.name}<span style={{ fontSize: "bolt" }}> {weather.data === null ? '' : weather.data.sys.country}</span></div>
+                    <div className="country">Longitude is: {weather.data === null ? '' : weather.data.coord.lon}</div>
+                    <div className="country">Latitude is:{weather.data === null ? '' : weather.data.coord.lat}</div>
                     <ButtonTable />
                 </div>
     );
 };
 
 
-export default connect()(TimeZone)
+export default TimeZone
