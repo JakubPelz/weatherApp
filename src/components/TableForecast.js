5f03e0c84e126d1fd60409d8cf3c9a2a5079be2b
Jakub Pelz <JakubMac@Jakub-MacBook-Air.local>
2021-10-31 14:47:42 +0100

    Table display 100%

--- src/components/TableForecast.js
+++ src/components/TableForecast.js
@@ -4,36 +4,34 @@ import { useSelector } from 'react-redux';
 
 const TableForecast = () => {
     const forecast = useSelector(state => state.forecast)
-    console.log(forecast)
     return (
         <div className="table">
             {forecast.data === null ? ('We are loading data from server ...') : (
-                <>
-                    <>
-                        {/*  <th>Date</th>
-                            <th>Night</th>
-                            <th>Day</th>
-                            <th>Sunrise</th>
-                            <th>Sunset</th> */}
-                        {forecast.data.daily.map((day, idx) => {
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Date</th>
+                            <th>Day Temp.</th>
+                            <th>Night Temp.</th>
+                            <th>Humidity</th>
+                            <th colspan="2">Picker Forecast</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {forecast.data.daily.map((item, idx) => {
                             return (
-                                <table>
-                                    <thead>
-                                        <tr>
-                                            <th key={idx}>Date</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <tr>
-                                            <td key={idx}>{moment(day.dt * 1000).format('dddd')}</td>
-                                        </tr>
-                                    </tbody>
-
-                                </table>
+                                <tr key={idx}>
+                                    <td key={item.id}>{moment(item.dt * 1000).format('L')}</td>
+                                    <td key={item.id}>{item.temp.day}&#176;C</td>
+                                    <td key={item.id}>{item.temp.night}&#176;C</td>
+                                    <td key={item.id}>{item.humidity} <span>%</span></td>
+                                    <td key={item.id}>{item.weather[0].main}</td>
+                                    <td key={item.id}><img src={`http://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`} alt="weather icon" className="w-icon" style={{width:"40px", height:"40px"}}/></td>
+                                </tr>                               
                             )
                         })}
-                    </>
-                </>
+                    </tbody>
+                </table>
             )}
         </div>
     )
